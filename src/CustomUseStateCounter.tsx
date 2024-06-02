b91f0791691fa5c877a5409471657470d6804bd6
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2024-06-02 18:37:47 +0100

    Extended useState implementation to handle multiple calls within same component instance

--- src/CustomUseStateCounter.tsx
+++ src/CustomUseStateCounter.tsx
@@ -1,47 +1,62 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-let stateValue: unknown;
+let callIndex = -1;
+const state: unknown[] = [];
+// let stateValue: unknown;
 let forceRender: () => void;
 
 // use extends in type parameter declaration to avoid confusing the TSX parser
 const customUseState = <T extends any>(initialValue: T): [T, (newValue: T) => void] => { 
-  if (stateValue === undefined) {
-    stateValue = initialValue;
+  callIndex += 1;
+  console.log('callIndex: ', callIndex);
+
+  const currentIndex = callIndex;
+
+  if (state[currentIndex] === undefined) {
+    state[currentIndex] = initialValue;
   }
 
   const setValue = (newValue: T) => {
     console.log('newvalue', newValue)
-    stateValue = newValue;
-    console.log('stateValue', stateValue)
+    state[currentIndex] = newValue;
+    console.log('stateValue', state[currentIndex])
     forceRender();
   }
 
-  return [stateValue as T, setValue];
+  return [state[callIndex] as T, setValue];
 }
 
 function Counter({ id }) {
-  const [count, setCount] = customUseState(0);
+  const [countA, setCountA] = customUseState(0);
+  const [countB, setCountB] = customUseState(0);
 
-  const handleIncrement = () => {
-    setCount(count + 1);
+  const handleIncrementA = () => {
+    setCountA(countA + 1);
+  }
+
+  const handleIncrementB = () => {
+    setCountB(countB + 1);
   }
 
   const [dummyState, setDummyState] = useState(0); // used to force re render
 
   forceRender = () => {
+    callIndex = -1;
     setDummyState((prev) => prev + 1);
   }
 
   useEffect(() => {
-    console.log(`CustomUseState counter rendered. count = ${count}`);
+    console.log(`CustomUseState counter rendered. countA = ${countA}. countB = ${countB}`);
   });
-
 
   return (
     <div>
-      <h1>Count {id}: {count}</h1>
-      <button onClick={handleIncrement}>Add</button>
+      <h1>Count A: {countA}</h1>
+      <button onClick={handleIncrementA}>Add</button>
+
+      <h1>Count B: {countB}</h1>
+      <button onClick={handleIncrementB}>Add</button>
     </div>
   )
 }
